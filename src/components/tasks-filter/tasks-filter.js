f8824bec100660ce0dc721e389672ce03ac58cea
Elena Grigoreva <gopan@inbox.ru>
2024-01-30 10:14:41 +0300

    update TasksFilter

--- src/components/tasks-filter/tasks-filter.js
+++ src/components/tasks-filter/tasks-filter.js
@@ -1,20 +1,26 @@
 import './tasks-filter.css'
 
 function TasksFilter({ filterBy, onFilterAll, onFilterActive, onFilterCompleted }) {
+  const filterClass = {
+    all: filterBy === 'all' ? 'selected' : '',
+    active: filterBy === 'active' ? 'selected' : '',
+    completed: filterBy === 'completed' ? 'selected' : '',
+  }
+
   return (
     <ul className="filters">
       <li>
-        <button type="button" className={filterBy === 'all' ? 'selected' : ''} onClick={onFilterAll}>
+        <button type="button" className={filterClass.all} onClick={onFilterAll}>
           All
         </button>
       </li>
       <li>
-        <button type="button" className={filterBy === 'active' ? 'selected' : ''} onClick={onFilterActive}>
+        <button type="button" className={filterClass.active} onClick={onFilterActive}>
           Active
         </button>
       </li>
       <li>
-        <button type="button" className={filterBy === 'completed' ? 'selected' : ''} onClick={onFilterCompleted}>
+        <button type="button" className={filterClass.completed} onClick={onFilterCompleted}>
           Completed
         </button>
       </li>
